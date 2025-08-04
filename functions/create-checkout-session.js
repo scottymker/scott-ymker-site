b45eccf8e47de25cfcf870c86efe9c50c7e2f76e
scottymker <scott@scottymkerphotos.com>
2025-08-04 11:33:53 -0500

    Updated create-checkout-session.js file

--- functions/create-checkout-session.js
+++ functions/create-checkout-session.js
@@ -1,39 +1,26 @@
-import Stripe from 'stripe';
 
 export async function onRequestPost(context) {
-  try {
-    const stripe = new Stripe(context.env.Stripe_Key, {
-      apiVersion: '2022-11-15',
-    });
+  const { request, env } = context;
+  const body = await request.json();
 
-    const body = await context.request.json();
+  const stripeRes = await fetch("https://api.stripe.com/v1/checkout/sessions", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${env.Stripe_Key}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      mode: "payment",
+      success_url: "https://schools.scottymkerphotos.com/success.html",
+      cancel_url: "https://schools.scottymkerphotos.com/cancel.html",
+      line_items: JSON.stringify(body.line_items),
+      payment_method_types: "card",
+      metadata: JSON.stringify(body.metadata),
+    }),
+  });
 
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      mode: 'payment',
-      line_items: body.items,
-      success_url: `${context.request.headers.get("origin")}/success.html`,
-      cancel_url: `${context.request.headers.get("origin")}/cancel.html`,
-      metadata: {
-        student_first_name: body.student.firstName,
-        student_last_name: body.student.lastName,
-        teacher: body.student.teacher,
-        grade: body.student.grade,
-        school: body.student.school,
-        parent_name: body.parent.name,
-        parent_phone: body.parent.phone,
-        parent_email: body.parent.email,
-        background: body.background
-      }
-    });
-
-    return new Response(JSON.stringify({ url: session.url }), {
-      headers: { 'Content-Type': 'application/json' }
-    });
-  } catch (err) {
-    return new Response(JSON.stringify({ error: err.message }), {
-      status: 500,
-      headers: { 'Content-Type': 'application/json' }
-    });
-  }
+  const session = await stripeRes.json();
+  return new Response(JSON.stringify({ id: session.id }), {
+    headers: { "Content-Type": "application/json" },
+  });
 }
